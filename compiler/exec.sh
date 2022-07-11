63c27f20e45d92aeac11b799858f6f7afca07d90
keicila.marcial <keicilamarcial2@gmail.com>
2022-07-11 00:41:43 -0300

    cleaning logs sh scrip

--- compiler/exec.sh
+++ compiler/exec.sh
@@ -4,18 +4,9 @@
 while :; do
         if [[ -f  data.csv && -f inputMap.map ]]; then
             java -jar netkorcompiler_teste.jar
+            rm data.csv
+            rm inputMap.map
         fi;
 
-        if [[ -f  data.csv ]]; then
-           rm data.csv
-           echo " data.csv is removed"
-        fi;
-
-        if [[ -f  inputMap.map ]]; then
-           rm data.csv
-           echo " inputMap.map is removed"
-        fi;
-
-        # echo "executing";
         sleep 1;
 done
