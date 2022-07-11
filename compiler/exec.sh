8f09677dc3c956b1091b2020d5a64c1ab766795a
keicila.marcial <keicilamarcial2@gmail.com>
2022-07-11 00:05:08 -0300

    Removing files after compiling them

--- compiler/exec.sh
+++ compiler/exec.sh
@@ -5,6 +5,17 @@ while :; do
         if [[ -f  data.csv && -f inputMap.map ]]; then
             java -jar netkorcompiler_teste.jar
         fi;
-        echo "executing";
+
+        if [[ -f  data.csv ]]; then
+           rm data.csv
+           echo " data.csv is removed"
+        fi;
+
+        if [[ -f  inputMap.map ]]; then
+           rm data.csv
+           echo " inputMap.map is removed"
+        fi;
+
+        # echo "executing";
         sleep 1;
 done
