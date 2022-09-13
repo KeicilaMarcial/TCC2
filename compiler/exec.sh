b11a88310ac97e90f6031213ba50ff6f5d35196a
keicila.marcial <keicilamarcial2@gmail.com>
2022-09-13 17:56:29 -0300

    refactoring

--- compiler/exec.sh
+++ compiler/exec.sh
@@ -4,8 +4,6 @@
 while :; do
         if [[ -f  data.csv && -f inputMap.map ]]; then
             java -jar netkorcompiler_teste.jar
-            rm data.csv
-            rm inputMap.map
         fi;
 
         sleep 1;
