4a017f66521b450231a763fe8bae32ce3bb479b4
keicila.marcial <keicilamarcial2@gmail.com>
2022-09-13 17:56:56 -0300

    delitting files after upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-const shell = require('shelljs');
+// const shell = require('shelljs');
 const app = express();
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const shell = require('child_process').execSync ;
 
+var zipper = require('zip-local');
 app.use(cors());
 
 const  upload = multer({storage: multer.diskStorage({
@@ -18,12 +20,33 @@ const  upload = multer({storage: multer.diskStorage({
     res.send({
       upload: true,
       files: req.files,
-    });
+    }
+    );
+    // apagando arquivos  com delay de 1 segundo
+    try {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        fs.unlinkSync('./compiler/data.csv')
+        fs.unlinkSync('./compiler/inputMap.map')
+
+      } catch(err) {
+        console.error(err)
+      }
   });
 
-app.get('/', (req, res)=>{
+app.get('/download',(req, res, next) => {
+    const src = './compiler';
+    const dist= '../files';
+    // shell(`mv ${src} ${dist}`);
+
+    zipper.sync.zip("./compiler/").compress().save("./compiler/pack.zip");
+    res.download('./compiler/pack.zip');
+    res.status(200);
+  });
+
+app.get('/', (req, res) =>{
     res.send('Hello World');
 });
+
 app.listen(5001,()=>{
     console.log('Back-end started! running on http://localhost:5001');
 });
